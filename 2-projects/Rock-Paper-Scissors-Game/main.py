43980b97dde4931cdaf64e1dd382b3d7444940b3
Iriome <iriome2512@gmail.com>
2026-01-19 11:36:50 +0000

    Save data in JSON in Rock Paper Scissors Game

--- 2-projects/Rock-Paper-Scissors-Game/main.py
+++ 2-projects/Rock-Paper-Scissors-Game/main.py
@@ -1,13 +1,30 @@
 #Rock, Paper, Scissors Game
 import random
+import os
+import json
 
 #Variables principales
 MOVEMENTS = ['rock', 'paper', 'scissors']
 WIN_RULES = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
 MAX_ATTEMPTS = 5
-
+FILE_NAME = "player_data.json"
 
 print("-----Welcome to Rock, Paper, Scissors Game!-----")
+
+"""Function to save progress"""
+def load_data():
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r", encoding="utf-8") as file:
+            return json.load(file)
+    else:
+        return {"money": 0}
+    
+"""Function to save data"""
+def save_data(data):
+    with open(FILE_NAME, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
 
 #Function to play a single round
 
@@ -27,15 +44,19 @@ def play_round():
         return "user"
     else:
         return "computer"
-    
+
 #Main game loop
+data = load_data()
+money = data["money"]
+
+print(f"You have: {money} money")
+
 user_score = 0
 computer_score = 0
 attempts = 0
 
 while attempts < MAX_ATTEMPTS:
     result = play_round()
-    
     if result is None:
         continue
     
@@ -56,17 +77,19 @@ while attempts < MAX_ATTEMPTS:
 print("\n-----Game Over-----")
 print(f"Final Scores - You: {user_score}, Computer: {computer_score}")
 
-money = 0
 
 if user_score > computer_score:
     money += 10
-    print(f"Congratulations! You won the game and earned ${money}.")
+    print("Congratulations! You won the game and earned $10.")
     
 elif computer_score > user_score:
     print("Computer won the game. Better luck next time!")
 else:
     print("The game ended in a tie! You earn $5 for your effort.")
     money += 5
-    
+
+data["money"] = money
+save_data(data)
+
     
 
